53a99ae63ba6465a9b2651b866d3e37ae95a147a
fortune <hosearamonjr@gmail.com>
2023-05-06 12:23:41 +0100

    git commit -m "hey"

--- js/stats.js
+++ js/stats.js
@@ -3,9 +3,7 @@ const container = document.querySelector(".section");
 
 async function initialNfts() {
   const dataFetch = await fetch(
-    `https://api.blockspan.com/v1/nfts?chain=eth-main&include_current_owners=true&include_recent_price=true&page_size=25`,
-    //"https://api.blockspan.com/v1/collections?chain=eth-main"
-    //"https://api.blockspan.com/v1/nfts/contract/0x41f56b000fffe17943fb4c182c123767af71d005?chain=eth-main&include_current_owners=true&include_recent_price=true&page_size=25",
+    `https://api.opensea.io/api/v1/assets?order_direction=desc&offset=0&limit=20`,
     {
       method: "GET",
       headers: {
@@ -16,23 +14,22 @@ async function initialNfts() {
   );
 
   const data = await dataFetch.json();
-  //console.log(data.results);
+  console.log(data);
 
-  data.results.forEach((result) => {
-    const metadata = result.metadata;
-
-    console.log(metadata.image);
-    console.log(result.token_name);
-
-    const nftImg = document.createElement("div");
-    nftImg.classList.add("nft-img");
-    nftImg.innerHTML = `
-      <img class="pic" src=${metadata.image}></img>
-      <h3>${result.token_name}</h3>
-      `;
-
-    container.appendChild(nftImg);
-  });
+  // data.results.forEach((result) => {
+  //   console.log(result.token_name);
+  //   const metadata = result.metadata;
+  //   console.log(metadata.image);
+  //   console.log(result.token_name);
+  //   const nftImg = document.createElement("div");
+  //   nftImg.classList.add("nft-img");
+  //   nftImg.innerHTML = `
+  //     <h3>${result}</h3>
+  //     `;
+  //   container.appendChild(nftImg);
+  // });
 }
 
 initialNfts();
+
+// <img class="pic" src=${metadata.image}></img>
